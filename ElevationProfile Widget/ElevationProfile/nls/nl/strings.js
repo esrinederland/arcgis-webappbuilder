59ddb1a62082794125bf2c3482503d590cb84612
Antoon Uijtdehaag <A.Uijtdehaag@esri.nl>
2015-06-22 13:23:52 +0200

    Added ElevationProfile Widget

---
+++ ElevationProfile Widget/ElevationProfile/nls/nl/strings.js
@@ -0,0 +1,26 @@
+define(({
+        descriptionlabel: "Teken een lijn op de kaart. Dubbelklik om het tekenen af te sluiten.",
+        chartLabel: "Beweeg over de hoogteprofiel grafiek om de locatie op de kaart te tonen.",
+        clear: "Wissen",
+        measurelabel: "Meten",
+        resultslabel: "Hoogteprofiel resultaat",
+        profileinfo: "Profiel Informatie",
+        display: {
+            elevationProfileTitle: "Hoogteprofiel",
+            hoverOver: "Beweeg over de hoogteprofiel grafiek om de locatie op de kaart te tonen."
+        },
+        chart: {
+            title: "Hoogteprofiel",
+            demResolution: "DEM Resolutie",
+            elevationTitleTemplate: "Hoogte in {0}",
+            distanceTitleTemplate: "Afstand in {0}",
+            gainLossTemplate: "Min: {min} Max: {max}<br>Start: {start} Einde: {end}<br>Verandering: {gainloss}"
+        },
+        errors: {
+            InvalidConfiguration: "Fout in de  configuratie.",
+            UnableToProcessResults: "Fout in verwerken analyse resultaten."
+        },
+        widgetversion: 'Hoogteprofiel Widget Versie Info',
+        widgetverstr: 'Widget Versie',
+        wabversionmsg: 'Widget is designed to run in Web AppBuilder version'
+ }));
